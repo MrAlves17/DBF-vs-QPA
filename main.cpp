d8d7b61ecf2cb4c77c967400b00c69cf449bc4a5
MrAlves17 <vitor_alves_barbosa@hotmail.com>
2021-06-01 20:02:05 -0300

    corrigindo o algoritmo uunifast

--- main.cpp
+++ main.cpp
@@ -1,30 +1,49 @@
-/*UUnifast algorithm*/
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <stdlib.h>
 
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<stdlib.h>
+#include <time.h>						// define time()
+#include "randomc/randomc.h"			// define classes for random number generators
 
-std::vector<int> UUniFast(int n, int u){
-	int nextSumU;
-	std::vector<int> vectU(n);
-	int sumU = u;
+#ifndef MULTIFILE_PROJECT
+// If compiled as a single file then include these cpp files, 
+// If compiled as a project then compile and link in these cpp files.
+   // Include code for the chosen random number generator:
+   #include "randomc/mersenne.cpp"
+   // define system specific user interface:
+   #include "randomc/userintf.cpp"
+#endif
+
+std::vector<float> UUniFast(int n, int u){
+
+	int seed = (int)time(0);			// random seed
+
+	CRandomMersenne RanGen(seed);		// make instance of random number generator
+
+	float nextSumU;
+	std::vector<float> utilizations(n);
+	float sumU = u;
 	for(int i=0; i<n-1;i++){
-		nextSumU = sumU*std::pow(rand(),1/(n-i));
-		vectU[i] = sumU - nextSumU;
+		nextSumU = sumU*std::pow(RanGen.Random(),1.0/(n-i));
+		utilizations[i] = sumU - nextSumU;
 		sumU = nextSumU;
 	}
 
-	vectU[n-1] = sumU;
+	utilizations[n-1] = sumU;
 
-	return vectU;
+	EndOfProgram();
+
+	return utilizations;
 }
 
 int main(){
 
-	std::vector<int> vectU = UUniFast(20, 1);
-	for(int i=0; i<vectU.size(); i++){
-		std::cout << vectU[i] << std::endl;
+	std::vector<float> utilizations = UUniFast(9, 1);
+	for(int i=0; i<utilizations.size(); i++){
+		std::cout << utilizations[i] << std::endl;
 	}
+
+
 	return 0;
 }
